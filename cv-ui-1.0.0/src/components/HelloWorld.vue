9074da7f7978ab897345074f64769d34ef32ee76
MrMoss12-cmd <eliashigueraacosta1@outlook.com>
2025-01-22 11:45:21 -0500

    integracion con bootsrap

--- cv-ui-1.0.0/src/components/HelloWorld.vue
+++ cv-ui-1.0.0/src/components/HelloWorld.vue
@@ -1,41 +1,16 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
+  <div class="container mt-5">
+    <h1 class="text-primary">¡Hola desde Vue 4 con Bootstrap!</h1>
+    <button class="btn btn-success mt-3">Haga clic aquí</button>
   </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
+<script>
+export default {
+  name: "HelloBootstrap",
+};
+</script>
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
+<style>
+/* Puedes agregar estilos personalizados aquí */
 </style>
